6d550b626fc5e1ef2d17951ec5c607e07a5d15a5
CribberSix <seifert.victor@web.de>
2020-12-28 13:36:55 +0100

    parameter cleanup

--- init.py
+++ init.py
@@ -20,7 +20,7 @@ clock = pygame.time.Clock()
 FPS = 30
 
 # ________________ GAME LOOP ________________ #
-nightly = StarryNight(systemWidth, systemHeight, 25, screen)
+nightly = StarryNight(25)
 
 col_background = (31, 71, 87)
 while True:
